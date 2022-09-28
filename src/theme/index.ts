54b65a65c16e7c223df6aeb658a767bd23f761bb
Mauricio Girardi <maurigirarde@yahoo.com.br>
2022-09-28 19:05:39 -0300

    improving the theme

---
+++ src/theme/index.ts
@@ -0,0 +1,19 @@
+import { dark } from "./dark";
+
+const colors = dark;
+
+export const themes = {
+  COLORS: { ...colors },
+
+  FONT_FAMILY: {
+    REGULAR: "Roboto_400Regular",
+    BOLD: "Roboto_700Bold",
+  },
+
+  FONT_SIZE: {
+    SM: 14,
+    MD: 16,
+    LG: 18,
+    XL: 24,
+  },
+};
---
+++ src/theme/light.ts
@@ -0,0 +1,16 @@
+export const light = {
+  GRAY_700: "#121214",
+  GRAY_600: "#F5F5F5",
+  GRAY_500: "#29292E",
+  GRAY_400: "#323238",
+  GRAY_300: "#7C7C8A",
+  GRAY_200: "#C4C4CC",
+  GRAY_100: "#E1E1E6",
+  WHITE: "#FFFFFF",
+
+  GREEN_500: "#00875F",
+  GREEN_400: "#00B37E",
+
+  RED_500: "#AA2834",
+  RED_400: "#F75A68",
+};
