8028d54c99d3c7d31ba72c3a8f9518249d47052c
Mauricio Girardi <maurigirarde@yahoo.com.br>
2022-09-28 19:06:18 -0300

    add fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,23 @@
-import { Groups } from "@screens/Groups";
 import { ThemeProvider } from "styled-components/native";
-import { dark } from "@theme/colors/dark";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
+import { Groups } from "@screens/Groups";
+import { themes } from "@theme/index";
+import { Loading } from "@components/Loading";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
   return (
-    <ThemeProvider theme={dark}>
-      <Groups />
+    <ThemeProvider theme={themes}>
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
