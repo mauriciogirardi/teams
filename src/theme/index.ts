9db45e7dd1b212077c5406fae0ce0359449c68a2
Mauricio Girardi <maurigirarde@yahoo.com.br>
2022-10-07 22:03:01 -0300

    change name theme

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -2,14 +2,13 @@ export const themes = {
   COLORS: {
     DARK: {
       BACKGROUND_PRIMARY: "#202024",
+      BACKGROUND_SECONDARY: "#121214",
       GRAY_PRIMARY: "#29292E",
       GRAY_SECOND: "#323238",
       WHITE: "#FFFFFF",
       GRAY_SUBTITLE: "#7C7C8A",
       GRAY_TITLE: "#C4C4CC",
       GRAY_TEXT: "#E1E1E6",
-
-      GRAY_700: "#121214",
 
       LOADING: "#A217E3",
 
@@ -22,14 +21,13 @@ export const themes = {
 
     LIGHT: {
       BACKGROUND_PRIMARY: "#FFFFFF",
+      BACKGROUND_SECONDARY: "#F1F1F1",
       GRAY_PRIMARY: "#F1F1F1",
       GRAY_SECOND: "#323238",
       WHITE: "#323238",
       GRAY_SUBTITLE: "#7C7C8A",
       GRAY_TITLE: "#323238",
       GRAY_TEXT: "#E1E1E6",
-
-      GRAY_700: "#121214",
 
       LOADING: "#A217E3",
 
