eb52683107c4dd0442927771a6fd42dc20e7b113
Mauricio Girardi <maurigirarde@yahoo.com.br>
2022-10-09 11:32:31 -0300

    add components in the screen players

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
+
+import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
 import { ButtonIcon } from "@components/ButtonIcon";
-import { Filter } from "@components/Filter";
-import { Header } from "@components/Header";
+import { PlayerCard } from "@components/PlayerCard";
 import { Highlight } from "@components/Highlight";
+import { ListEmpty } from "@components/ListEmpty";
+import { Filter } from "@components/Filter";
+import { Button } from "@components/Button";
+import { Header } from "@components/Header";
 import { Input } from "@components/Input";
-import { Container, Form } from "./styles";
 
 export function Players() {
+  const [team, setTeam] = useState("");
+  const [players, setPlayers] = useState([]);
+
   return (
     <Container>
       <Header showBackButton />
@@ -14,12 +23,46 @@ export function Players() {
         subtitle="adicione a galera e separe os times."
       />
 
-      <Filter title="filtro" isActive />
-
       <Form>
         <Input placeholder="Nome da pessoa" autoCorrect={false} />
         <ButtonIcon icon="add" />
       </Form>
+
+      <HeaderList>
+        <FlatList
+          data={["Time A", "Time B", "Time C", "Time D"]}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <Filter
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+        />
+
+        <NumberOfPlayers>{players.length}</NumberOfPlayers>
+      </HeaderList>
+
+      <FlatList
+        data={players}
+        showsVerticalScrollIndicator={false}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <PlayerCard name={item} onRemove={() => {}} />
+        )}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Não há pessoas nesse time" />
+        )}
+        contentContainerStyle={[
+          { paddingBottom: 100 },
+          players.length === 0 && { flex: 1 },
+        ]}
+      />
+
+      <Button type="secondary">Remover Turma</Button>
     </Container>
   );
 }
--- src/screens/Players/styles.ts
+++ src/screens/Players/styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
@@ -12,4 +12,20 @@ export const Form = styled.View`
   justify-content: center;
   flex-direction: row;
   border-radius: 6px;
+`;
+
+export const HeaderList = styled.View`
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  margin: 32px 0 12px;
+`;
+
+export const NumberOfPlayers = styled.Text`
+  ${({ theme }) =>
+    css({
+      color: theme.COLORS.GRAY_TEXT,
+      fontFamily: theme.FONT_FAMILY.BOLD,
+      fontSize: `${theme.FONT_SIZE.SM}pc`,
+    })}
 `;
