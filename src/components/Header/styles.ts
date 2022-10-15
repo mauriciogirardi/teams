378cf31044c89cb8db85cacc4d6beccc5cd4c03d
Mauricio Girardi <maurigirarde@yahoo.com.br>
2022-10-15 14:45:39 -0300

    Add SafeAreaView

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -6,7 +6,6 @@ export const Container = styled.View`
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
-  margin-top: 20px;
 `;
 
 export const Logo = styled.Image`
--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -1,6 +1,7 @@
 import styled from "styled-components/native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-export const Container = styled.View`
+export const Container = styled(SafeAreaView)`
   flex: 1;
   padding: 24px;
 
